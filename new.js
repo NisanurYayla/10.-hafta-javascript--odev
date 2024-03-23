b0e8110632bb9a71c532b97e06083c6f158cc8e5
GolgeMuhendis <nisanuryayla13@gmail.com>
2024-03-23 06:48:05 +0300

    homework completed

---
+++ new.js
@@ -0,0 +1,85 @@
+let a="Word";
+console.log(a);
+
+let b=9;
+console.log(b);
+
+let value1="Merhaba";
+let uzunluk=value1.length;
+console.log(uzunluk);
+
+let value2=5;
+console.log(value2+5);
+
+console.log(b+value2);
+console.log(b-value2);
+console.log(b*value2);
+console.log(b/value2);
+
+
+let str1="Merhaba";
+let str2="Dunya";
+console.log(str1+str2);
+
+let mantıksalesitlik=(str1==str2);
+console.log(mantıksalesitlik);
+
+if(value2>10){
+    console.log("Sayı 10'dan buyuktur.");
+}
+
+else{
+    console.log("Sayı 10'dan kucuktur.");
+}
+
+if(uzunluk==5){
+    console.log("String 5 karakterden olusuyor");
+}
+
+else{
+    console.log("String bes karakterden farklıdır");
+}
+
+let switchyapısı=7;
+
+switch(switchyapısı){
+    case 1:
+        console.log("Bir");
+        break;
+
+    case 2:
+        console.log("Iki");
+        break;
+
+    case 3:
+        console.log("Uc");
+        break;
+
+    case 4:
+        console.log("Dort");
+        break;
+
+    case 5:
+        console.log("Bes");
+        break;
+
+    case 6:
+        console.log("Altı");
+        break;
+
+    case 7:
+        console.log("Yedi");
+        break;
+
+    default:
+        console.log("Gecersiz sayı girdiniz.");
+        break;
+
+
+        
+
+        
+
+        
+
+}
